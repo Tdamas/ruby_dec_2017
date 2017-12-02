b0d076c9655947e5ad3b8107075c4a1d7d517adf
Tamisha <tamishadamas@gmail.com>
2017-12-02 12:17:23 -0600

    Completed blog_posts_messages-2

--- Tamisha_Damas/Rails/Models /blog_posts_messages_2/app/models/message.rb
+++ Tamisha_Damas/Rails/Models /blog_posts_messages_2/app/models/message.rb
@@ -2,5 +2,5 @@ class Message < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
 
-  validates :author, :messages, presence: true, length: { minimum: 3 }
+  validates :author, :message, presence: true, length: { minimum: 3 }
 end
